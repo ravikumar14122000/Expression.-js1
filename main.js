1d2eb52de04201a1bac08f09381a417cda342ece
ravimari1410 <ponravikumar2000@gmail.com>
2023-03-08 16:22:17 +0530

    add

---
+++ main.js
@@ -0,0 +1,80 @@
+var x=15, y=20, z=5;
+console.log(x+y-z);
+
+var a=10, b=20, c=2, d=15;
+console.log(a+b/c-d);
+
+var  a=6, b=12, c=18, d=24;
+console.log(a*b+c-d);
+
+var a=5, b=10, c=15;
+console.log(a+b%c);
+
+var a=20, b=10, c=15;
+console.log(a-b+c);
+
+var a=2, b=5, c=15, d=10;
+console.log(b/a+c-d);
+
+var a=10, b=10, c=20;
+console.log(++c);
+console.log(++a);
+console.log(b++);
+
+var m=10, s=20, p=15;
+console.log(--m *s+p);
+
+var m=10;
+console.log(m--);
+
+
+var m=10;
+console.log(m--);
+
+//comparison operators
+
+var e=5, s=10, a=25, d=15;
+console.log(a<s);
+
+console.log(a>d);
+console.log(a<=d);
+console.log(a==e);
+console.log(e!=a);
+console.log(d!==s);
+console.log(a<=d);
+
+//Expression operaors
+
+var a=1, b=2, c=5, d=3, e=5;
+console.log(a+(a+b)-c*d);
+
+console.log(e-(a*b)+e+c);
+
+console.log(a+b-(b*e)+c-a);
+
+var a=10, b=2, c=5, d=3, e=5;
+console.log(a-b+(c*a)+a/e);
+
+
+var x=10, y=20, z=15;
+
+console.log(x/y+z-x*(x-z));
+
+var a=5, b=5, c=10;
+console.log(a==b)||(c<b);
+
+var a=1, b=2, c=3;
+console.log(a*b)&&(b*C)&&(c*a);
+
+var a=1, b=2, c=3;
+console.log(!(a=b)&&(B==c));
+
+
+var a=30, b=5;
+console.log((a++)+(++a)-(a+b));
+
+console.log((a*b)+(a%b)-(a+b));
+
+document.write((a++)+(a-b)-(++a));
+
+console.log((a-b)+(a*b)+(a/b));
